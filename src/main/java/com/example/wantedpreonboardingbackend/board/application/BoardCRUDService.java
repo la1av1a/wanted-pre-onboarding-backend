f879b9822bec09cdcc2cf2e7824d887f78becd61
la1av1a <zldqkrktqk@gmail.com>
2023-08-16 17:21:11 +0900

    feat : 게시글 목록을 조회하는 엔드포인트 구현

--- src/main/java/com/example/wantedpreonboardingbackend/board/application/BoardCRUDService.java
+++ src/main/java/com/example/wantedpreonboardingbackend/board/application/BoardCRUDService.java
@@ -3,6 +3,10 @@ package com.example.wantedpreonboardingbackend.board.application;
 import com.example.wantedpreonboardingbackend.board.domain.Board;
 import com.example.wantedpreonboardingbackend.board.domain.BoardRepository;
 import lombok.RequiredArgsConstructor;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Sort.Direction;
+import org.springframework.data.web.PageableDefault;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
@@ -25,5 +29,10 @@ public class BoardCRUDService {
     @Transactional(readOnly = true)
     public Board findBoardByBoardIdAndMemberId(Long boardId,Long memberId){
         return boardRepository.findBoardByIdAndAuthorMemberId(boardId,memberId).orElseThrow(() -> new IllegalArgumentException("게시글을 찾을 수 없습니다."));
+    }
+
+    @Transactional(readOnly = true)
+    public Page<Board> findBoardList(Pageable pageable){
+        return boardRepository.findAll(pageable);
     }
 }
--- src/main/java/com/example/wantedpreonboardingbackend/board/presentation/BoardController.java
+++ src/main/java/com/example/wantedpreonboardingbackend/board/presentation/BoardController.java
@@ -2,11 +2,16 @@ package com.example.wantedpreonboardingbackend.board.presentation;
 
 import com.example.wantedpreonboardingbackend.board.application.BoardService;
 import com.example.wantedpreonboardingbackend.board.presentation.dto.BoardFindingResponseDTO;
+import com.example.wantedpreonboardingbackend.board.presentation.dto.BoardListResponseDTO;
 import com.example.wantedpreonboardingbackend.board.presentation.dto.BoardPostingRequestDTO;
 import com.example.wantedpreonboardingbackend.common.response.ResponseDTO;
 import com.example.wantedpreonboardingbackend.common.security.CustomUserDetails;
 import jakarta.validation.Valid;
+import java.util.List;
 import lombok.RequiredArgsConstructor;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Sort.Direction;
+import org.springframework.data.web.PageableDefault;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
@@ -58,5 +63,10 @@ public class BoardController {
         boardService.updateBoard(boardId,title,content,Long.parseLong(userDetails.getId()));
 
         return new ResponseEntity<>(ResponseDTO.ofSuccess("게시글 수정에 성공하였습니다."), HttpStatus.OK);
+    }
+
+    @GetMapping("/")
+    public ResponseEntity<ResponseDTO<List<BoardListResponseDTO>>> getBoardList(@PageableDefault(sort = "id",direction = Direction.DESC) Pageable pageable){
+        return new ResponseEntity<>(new ResponseDTO<>(boardService.findBoardList(pageable),"게시판 조회에 성공하였습니다."), HttpStatus.OK);
     }
 }
